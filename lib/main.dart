538f1204c77891e924da1af95907d090ceeda79b
Vuthy_Tourn <vuthytuon168@gmail.com>
2026-04-02 12:07:30 +0700

    Homework is done

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   final url =
-      "https://img.freepik.com/free-photo/lavender-field-sunset-near-valensole_268835-3910.jpg?semt=ais_incoming&w=740&q=80";
+      "https://whc.unesco.org/uploads/thumbs/site_0798_0006-1000-627-20120827171714.jpg";
+  final sundarban =
+      '''The Sundarbans mangrove forest, one of the largest such forests in the world (140,000 ha),lies on the delta of the Ganges, Brahmaputra and Meghna rivers on the Bay of Bengal. It is adjacent to the border of India’s Sundarbans World Heritage site inscribed in 1987. 
+The site is intersected by a complex network of tidal waterways, mudflats and small islands of salt-tolerant mangrove forests, and presents an excellent example of ongoing ecological processes.
+The area is known for its wide range of fauna, including 260 bird species, the Bengal tiger and other threatened species such as the estuarine crocodile and the Indian python.''';
 
   // This widget is the root of your application.
   @override
@@ -26,20 +30,47 @@ class MyApp extends StatelessWidget {
             children: [
               Image.network(url, fit: BoxFit.cover),
               Container(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("Landscape", style: TextStyle(color: Colors.deepPurpleAccent, fontWeight: FontWeight.w500, fontSize: 15),),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "The Sundarban",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                        ),
+                        Text(
+                          "Khulna Division, Bagladesh",
+                          style: TextStyle(color: Colors.grey, fontSize: 14),
+                        ),
+                      ],
+                    ),
+
                     Row(
                       children: [
-                        ...List.generate(
-                          5,
-                          (index) =>
-                              Icon(Icons.star, color: Colors.amber, size: 20),
+                        SizedBox(
+                          height: 40,
+                          child: VerticalDivider(
+                            color: Colors.grey,
+                            thickness: 1,
+                          ),
                         ),
                         SizedBox(width: 8),
-                        Text("4.5"),
+
+                        Column(
+                          children: [
+                            Icon(Icons.star, color: Colors.amber, size: 20),
+                            Text(
+                              "9.5",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ],
@@ -50,41 +81,64 @@ class MyApp extends StatelessWidget {
                 children: [
                   Container(
                     padding: EdgeInsets.all(20),
-                    color: Colors.grey[400],
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.white, width: 2),
+                    ),
                     child: Column(
                       children: [
-                        Icon(Icons.headphones, color: Colors.blue),
-                        Text("Headphone", style: TextStyle(color: Colors.blue)),
+                        Icon(Icons.call, color: Colors.blue),
+                        SizedBox(height: 5),
+                        Text("CALL", style: TextStyle(color: Colors.blue)),
                       ],
                     ),
                   ),
                   Container(
                     padding: EdgeInsets.all(20),
-                    color: Colors.grey[400],
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.white, width: 2),
+                    ),
                     child: Column(
                       children: [
-                        Icon(Icons.headphones, color: Colors.blue),
-                        Text("Headphone", style: TextStyle(color: Colors.blue)),
+                        Icon(Icons.alt_route, color: Colors.blue),
+                        SizedBox(height: 5),
+                        Text("ROUTE", style: TextStyle(color: Colors.blue)),
                       ],
                     ),
                   ),
                   Container(
                     padding: EdgeInsets.all(20),
-                    color: Colors.grey[400],
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.white, width: 2),
+                    ),
                     child: Column(
                       children: [
-                        Icon(Icons.headphones, color: Colors.blue),
-                        Text("Headphone", style: TextStyle(color: Colors.blue)),
+                        Icon(Icons.share, color: Colors.blue),
+                        SizedBox(height: 5),
+                        Text("SHARE", style: TextStyle(color: Colors.blue)),
                       ],
                     ),
                   ),
                 ],
               ),
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                 child: Text(
-                  "A landscape comprises the visible features of land—including mountains, water, and vegetation—integrating natural and human-made elements for aesthetic or functional purposes. It represents both natural scenery and designed outdoor spaces, such as gardens, parks, or urban environments, often managed for ecology and usability. ",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                  sundarban,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey,
+                  ),
                 ),
               ),
             ],
@@ -92,9 +146,9 @@ class MyApp extends StatelessWidget {
         ),
         appBar: AppBar(
           title: Text(
-            "Simple App",
+            "Sundarban",
             style: TextStyle(
-              color: Colors.red,
+              color: Colors.green,
               fontSize: 24,
               fontWeight: FontWeight.bold,
             ),
